72f2e3b9602a369fd88cc200f651d1bd14ea0d09
VaideviShetty <110751028+VaideviShetty@users.noreply.github.com>
2022-08-07 08:20:42 +0530

    Add files via upload
    day1

---
+++ day1/Car.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Car {
+
+	            public void applybreak() {
+	            	System.out.println("Apply break");
+	            }
+	            public void soundhorn() {
+	            	System.out.println("Sound Horn");
+	            }
+	
+	
+	public static void main(String[] args) {
+	           Car Bikeclass =new Car();
+	           Bikeclass.applybreak();
+	           Bikeclass.soundhorn();
+	}
+
+}
---
+++ day1/Student.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Student {
+
+	public static void main(String[] args) {
+		  String studentName= "vaidevi";
+		  short rollNo= 112;
+		  String collegeName= "JNTUK";
+          int markScored= 956;
+          float ccpa= 9.5F;
+          
+          System.out.println(studentName);
+          System.out.println(rollNo);
+          System.out.println(collegeName);
+          System.out.println(markScored);
+          System.out.println(ccpa);
+	}
+
+}
